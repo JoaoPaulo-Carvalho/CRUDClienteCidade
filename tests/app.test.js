9fdd6b71d4acef7c72865caa4755dd529bca275e
Joao Paulo Araujo <araujo.jpc@outlook.com>
2021-02-13 13:43:02 -0300

    [Teste][Cliente] Método POST

--- tests/app.test.js
+++ tests/app.test.js
@@ -31,7 +31,7 @@ describe('Teste metodo POST de cidades', () => {
     'Status 409 - uf == \'\'',
     () => request(app)
       .post('/api/cidades')
-      .send({ nome: 'Juiz de Fora' })
+      .send({ nome: 'Juiz de Fora', uf: '' })
       .expect(409),
   );
 
@@ -40,6 +40,57 @@ describe('Teste metodo POST de cidades', () => {
     () => request(app)
       .post('/api/cidades')
       .send({ uf: 'MG' })
+      .expect(409),
+  );
+});
+
+describe('Teste metodo POST de clientes', () => {
+  test(
+    'Status 201 - dados completos',
+    () => request(app)
+      .post('/api/clientes')
+      .send({
+        nome: 'João Paulo',
+        sexo: 'M',
+        dataNasc: '1997-02-03',
+        idade: 24,
+        cidadeID: '6ccf2dff-4ed3-4fb7-b5f7-4c9fcd179aa8',
+      })
+      .expect(201),
+  );
+
+  test(
+    'Status 409 - sem dados',
+    () => request(app)
+      .post('/api/clientes')
+      .send({})
+      .expect(409),
+  );
+
+  test(
+    'Status 409 - dataNasc == \'\'',
+    () => request(app)
+      .post('/api/clientes')
+      .send({
+        nome: 'João Paulo',
+        sexo: 'M',
+        dataNasc: '',
+        idade: 24,
+        cidadeID: '6ccf2dff-4ed3-4fb7-b5f7-4c9fcd179aa8',
+      })
+      .expect(409),
+  );
+
+  test(
+    'Status 409 - faltando nome',
+    () => request(app)
+      .post('/api/clientes')
+      .send({
+        sexo: 'M',
+        dataNasc: '',
+        idade: 24,
+        cidadeID: '6ccf2dff-4ed3-4fb7-b5f7-4c9fcd179aa8',
+      })
       .expect(409),
   );
 });
