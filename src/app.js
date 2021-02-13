efd39e0800763377cbf3ee7cd0f4235bc3e136a7
Joao Paulo Araujo <araujo.jpc@outlook.com>
2021-02-13 12:08:15 -0300

    [Geral] Modularizando index.js

--- src/app.js
+++ src/app.js
@@ -1,41 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+const pool = require('./database');
 
 const app = express();
 const router = express.Router();
-const port = process.env.port || 3000;
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432,
-});
-
-pool.query(
-  `
-  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-  
-  CREATE TABLE IF NOT EXISTS public.cidade (
-    id uuid NOT NULL PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
-    nome character varying NOT NULL,
-    uf character varying NOT NULL
-  );
-  
-  CREATE TABLE IF NOT EXISTS public.cliente (
-    id uuid NOT NULL PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
-    nome character varying NOT NULL,
-    sexo character varying NOT NULL,
-    data_nasc date NOT NULL,
-    idade integer NOT NULL,
-    cidade_id uuid NOT NULL,
-    CONSTRAINT cliente_cidade_id_fkey
-      FOREIGN KEY (cidade_id)
-        REFERENCES public.cidade(id) ON UPDATE CASCADE ON DELETE RESTRICT
-  );`,
-);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -76,4 +44,4 @@ router.post('/clientes', (req, res) => {
   });
 });
 
-app.listen(port);
+module.exports = app;
