39a1f8c20b3e3d1be55775782092b61ec633c0cf
Joao Paulo Araujo <araujo.jpc@outlook.com>
2021-02-13 10:34:31 -0300

    [API] Processando o body com Body Parser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 const port = process.env.port || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/api', router);
 
 router.get('/', (req, res) => {
   res.send('API funcionando pelo router!');
+});
+
+router.post('/cidades', (req, res) => {
+  res.status(200).json(req.body);
 });
 
 app.listen(port);
