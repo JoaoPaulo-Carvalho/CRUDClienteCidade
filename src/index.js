988d18d99a8c664b7199ac6bc2df436c9246a2b3
Joao Paulo Araujo <araujo.jpc@outlook.com>
2021-02-13 11:20:05 -0300

    [PostgreSQL] Conexão e criação de tabelas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
 
 const app = express();
 const router = express.Router();
 const port = process.env.port || 3000;
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'postgres',
+  port: 5432,
+});
+
+pool.query(
+  `CREATE TABLE IF NOT EXISTS public.cidade (
+    id uuid NOT NULL PRIMARY KEY UNIQUE,
+    nome character varying NOT NULL,
+    uf character varying NOT NULL
+  );
+  
+  CREATE TABLE IF NOT EXISTS public.cliente (
+    id uuid NOT NULL PRIMARY KEY UNIQUE,
+    nome character varying NOT NULL,
+    sexo character varying NOT NULL,
+    data_nasc date NOT NULL,
+    idade integer NOT NULL,
+    cidade_id uuid NOT NULL,
+    CONSTRAINT cliente_cidade_id_fkey
+      FOREIGN KEY (cidade_id)
+        REFERENCES public.cidade(id) ON UPDATE CASCADE ON DELETE RESTRICT
+  );`,
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
