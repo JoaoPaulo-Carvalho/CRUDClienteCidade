e70c8b2830fb177b4a84ba714e8ee1e3e0108002
Joao Paulo Araujo <araujo.jpc@outlook.com>
2021-02-13 13:34:01 -0300

    [Teste][Cidade] Método POST

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,3 +9,37 @@ describe('Testar a raiz do router', () => {
       .expect(200),
   );
 });
+
+describe('Teste metodo POST de cidades', () => {
+  test(
+    'Status 201 - dados completos',
+    () => request(app)
+      .post('/api/cidades')
+      .send({ nome: 'Juiz de Fora', uf: 'MG' })
+      .expect(201),
+  );
+
+  test(
+    'Status 409 - sem dados',
+    () => request(app)
+      .post('/api/cidades')
+      .send({})
+      .expect(409),
+  );
+
+  test(
+    'Status 409 - uf == \'\'',
+    () => request(app)
+      .post('/api/cidades')
+      .send({ nome: 'Juiz de Fora' })
+      .expect(409),
+  );
+
+  test(
+    'Status 409 - faltando nome',
+    () => request(app)
+      .post('/api/cidades')
+      .send({ uf: 'MG' })
+      .expect(409),
+  );
+});
